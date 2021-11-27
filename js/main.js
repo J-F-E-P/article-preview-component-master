4aba969aad2501f47fc04fba5ab550a09b32c428
Jose Esteves <josefelixestevenpacheco@gmail.com>
2021-11-26 20:18:43 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const btnRedes = document.getElementById("btnRedes");
+btnRedes.addEventListener('click',(e)=>{
+  if (e.target.classList.contains('card__btnRedes')) {
+    e.target.classList.toggle("card__btnRedes--show")
+    e.target.firstElementChild.lastElementChild.classList.toggle("card__iconoShareRedes--show")
+    e.target.nextElementSibling.classList.toggle("card__contenRedes--show")
+  console.log(e)
+  }
+})
